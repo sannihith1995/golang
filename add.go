f76afba61b3c1ac89e8218c2fed72235108e7fb5
sannihith1995 <sannihith.1995@gmail.com>
2017-05-12 14:28:25 +0530

    Update add.go

--- add.go
+++ add.go
@@ -3,9 +3,9 @@ package main
 import "fmt"
 
 func main(){
-var a,b,c int
-fmt.Println("Enter two numbers : ")
-fmt.Scan(&a,&b)
-c=a+b
-fmt.Println("The sum is : ",c)
+  var a,b,c int
+  fmt.Println("Enter two numbers : ")
+  fmt.Scan(&a,&b)
+  c=a+b
+  fmt.Println("The sum is : ",c)
 }
